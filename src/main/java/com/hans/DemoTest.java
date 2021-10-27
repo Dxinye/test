a9e69b84e62ad3f57c23079f1c53609902416ac1
zengshengqiang@37.com <zengshengqiang@37.com>
2021-10-27 10:12:31 +0800

    test

---
+++ src/main/java/com/hans/DemoTest.java
@@ -0,0 +1,22 @@
+package com.hans;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * 公司：三七互娱
+ * 作者：曾声强
+ * 所属包 : com.hans
+ * 创建日期：2021/10/9 时间：19:49
+ * 更新日期：2021/10/9 时间：19:49
+ * 描述：这个类的主要功能及作用说明
+ */
+public class DemoTest {
+
+    public static void main(String[] args){
+
+
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("format");
+        return Integer.valueOf(simpleDateFormat.format(date));
+    }
+}
